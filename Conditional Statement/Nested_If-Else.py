e01ccda07a9085a4cbeea060be605e636b32d606
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-12-31 21:35:58 +0600

    Add files via upload

---
+++ Conditional Statement/Nested_If-Else.py
@@ -0,0 +1,8 @@
+num = 10
+if num > 0:
+    if num % 2 == 0:
+        print("The number is positive and even.")
+    else:
+        print("The number is positive and odd.")
+else:
+    print("The number is not positive.")
---
+++ Conditional Statement/Simple_if.py
@@ -0,0 +1,3 @@
+num = 5
+if num > 0:
+    print("The number is positive.")
