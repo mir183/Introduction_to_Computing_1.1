e15ccc8c656e669ff0180d7f9e847d2883350241
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-12-31 21:15:16 +0600

    Add files via upload

---
+++ Input Output and Comments/comments.py
@@ -0,0 +1,22 @@
+# Input example
+user_name = input("Enter your name: ")
+
+# Output example
+print("Hello, " + user_name + "! Welcome to Python.")
+
+# Formatted Output example using f-strings
+age = input("Enter your age: ")
+print(f"Hello, {user_name}! You are {age} years old.")  # Output with user's name and age
+
+# Another way of formatted output using format() method
+print("Hello, {}! You are {} years old.".format(user_name, age))  # Output using format() method
+
+#This is a single line comment.
+
+"""
+This is a multi-line comment.
+It provides additional information about the code.
+In this case, it explains the usage of different types of comments.
+"""
+
+# End of the code
---
+++ Input Output and Comments/input_output.py
@@ -0,0 +1,12 @@
+# Input example
+user_name = input("Enter your name: ")
+
+# Output example
+print("Hello, " + user_name + "! Welcome to Python.")
+
+# Formatted Output example
+age = input("Enter your age: ")
+print(f"Hello, {user_name}! You are {age} years old.")
+
+# Another way of formatted output using format() method
+print("Hello, {}! You are {} years old.".format(user_name, age))
