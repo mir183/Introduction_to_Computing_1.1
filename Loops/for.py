dba23855fd362c1ade2008d45cd3ea2dc60b3571
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-12-31 21:42:01 +0600

    Add files via upload

---
+++ Loops/for.py
@@ -0,0 +1,7 @@
+# A list of fruits, initialization is important
+fruits = ["apple", "banana", "cherry", "durian", "elderberry", "fig", "grape", "honeydew"]
+
+# A 'for' loop to go through each fruit in the list
+for fruit in fruits:
+    # Print the current fruit
+    print(fruit)
---
+++ Loops/while.py
@@ -0,0 +1,10 @@
+# Initialization, this is important
+i = 1
+
+# Condition
+while i <= 10:
+    # Code to execute while the condition is true
+    print(i)
+
+    # Increment
+    i += 1
