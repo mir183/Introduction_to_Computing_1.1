d5267d705eebb3c66b4491d2521956af740ed282
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-12-31 21:07:39 +0600

    Add files via upload

---
+++ Variable and Data Type/datatype.py
@@ -0,0 +1,33 @@
+# Integer data type
+num_integer = 10
+
+# Float data type
+num_float = 3.14
+
+# String data type
+text_string = "Hello, Python!"
+
+# Boolean data type
+is_true = True
+
+# List data type
+my_list = [1, 2, 3, 4]
+
+# Tuple data type
+my_tuple = (5, 6, 7, 8)
+
+# Set data type
+my_set = {1, 2, 3, 4}
+
+# Dictionary data type
+my_dict = {'name': 'John', 'age': 25, 'city': 'Example City'}
+
+# Printing the data types
+print("Integer data type:", type(num_integer))
+print("Float data type:", type(num_float))
+print("String data type:", type(text_string))
+print("Boolean data type:", type(is_true))
+print("List data type:", type(my_list))
+print("Tuple data type:", type(my_tuple))
+print("Set data type:", type(my_set))
+print("Dictionary data type:", type(my_dict))
---
+++ Variable and Data Type/variable.py
@@ -0,0 +1,33 @@
+# Integer variable
+num_integer = 10
+
+# Float variable
+num_float = 3.14
+
+# String variable
+text_string = "Hello, Python!"
+
+# Boolean variable
+is_true = True
+
+# List variable
+my_list = [1, 2, 3, 4]
+
+# Tuple variable
+my_tuple = (5, 6, 7, 8)
+
+# Set variable
+my_set = {1, 2, 3, 4}
+
+# Dictionary variable
+my_dict = {'name': 'John', 'age': 25, 'city': 'Example City'}
+
+# Printing the variables
+print("Integer:", num_integer)
+print("Float:", num_float)
+print("String:", text_string)
+print("Boolean:", is_true)
+print("List:", my_list)
+print("Tuple:", my_tuple)
+print("Set:", my_set)
+print("Dictionary:", my_dict)
